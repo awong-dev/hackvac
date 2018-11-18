fc3f34d37071ed5c0e8ce5dd7202f255499330d7
Albert J. Wong <awong.dev@gmail.com>
2018-11-18 13:39:10 -0800

    Setup Mongoose component with a clean submodule a more sensible build.

---
+++ src/components/mongoose/include/mongoose.h
@@ -0,0 +1,16 @@
+#ifndef ESP_MONGOOSE_WRAPPER_H_
+#define ESP_MONGOOSE_WRAPPER_H_
+
+// Handle to ensure all users of his component have the same configuration
+// for mongoose. This ideally would be done by having a setting in
+// components.mk that allowed setting CFLAGS/CXXFLAGS/CPPFLAGS for all users
+// of the component, but that does not exist.
+//
+// KEEP THIS IN SYNC WITH components.mk OR THERE WILL BE UNDEFINED BEHAVIOR.
+#define MG_ENABLE_HTTP_STREAMING_MULTIPART 1
+#define MG_ENABLE_HTTP_WEBSOCKET 1
+#define MG_ENABLE_BROADCAST 1
+
+#include "../src/mongoose.h"
+
+#endif  // ESP_MONGOOSE_WRAPPER_H_

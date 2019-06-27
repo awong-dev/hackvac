04121e8b7e956e3a24bc54470dc557f99cf5fc39
Albert J. Wong <awong.dev@gmail.com>
2019-06-26 23:21:13 -0700

    Delete all of esp_cxx to move to separate module

--- src/components/esp_cxx/component.mk
+++
@@ -1,2 +0,0 @@
-CXXFLAGS += -std=c++17
-COMPONENT_SRCDIRS = src src/httpd src/firebase
--- src/components/esp_cxx/test/component.mk
+++
@@ -1,6 +0,0 @@
-CXXFLAGS += -std=c++17
-ifeq ($(shell uname -s),Darwin)
-COMPONENT_ADD_LDFLAGS = -Wl,-force_load $(COMPONENT_BUILD_DIR)/$(COMPONENT_LIBRARY)
-else
-COMPONENT_ADD_LDFLAGS = -Wl,--whole-archive -l$(COMPONENT_NAME) -Wl,--no-whole-archive
-endif
